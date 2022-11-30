c7797f370fd75909d820c5cdc3010048ee6e774c
PatrickVongsa <patrick.vongsa@gmail.com>
2022-11-30 07:48:12 +0100

    forgotten file

--- client/src/components/header/Header.component.tsx
+++ client/src/components/header/Header.component.tsx
@@ -1,30 +1,39 @@
-import React from 'react'
+import React from "react";
 
-import './header.styles.css'
+import "./header.styles.css";
 
 interface IProps {
-    pageTitle: string;
+  pageTitle: string;
+  searchBar: boolean;
+  createButton: boolean;
+  tabs: boolean;
 }
 
-function Header({pageTitle}: IProps) {
+function Header({ pageTitle, searchBar, createButton, tabs }: IProps) {
   return (
-    <div className='header'>
-        <h1>{pageTitle}</h1>
-        <div className='container'>
-            <div>
-                <input type="search" name="search" id="search" />
-            </div>
-            <div>
-                <button>+ Créer</button>
-            </div>
-            <div>
-                <button>Vue Liste</button>
-                <button>Vue Colonnes</button>
-                <button>Archivés</button>
-            </div>
-        </div>
+    <div className="header">
+      <h1>{pageTitle}</h1>
+      <div className="container">
+        {searchBar && (
+          <div>
+            <input type="search" name="search" id="search" />
+          </div>
+        )}
+        {createButton && (
+          <div>
+            <button>+ Créer</button>
+          </div>
+        )}
+        {tabs && (
+          <div>
+            <button>Vue Liste</button>
+            <button>Vue Colonnes</button>
+            <button>Archivés</button>
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
