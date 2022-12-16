7cee34815e0bc5c61f00cbeda3a3e0a5fdf1da5f
PatrickVongsa <patrick.vongsa@gmail.com>
2022-12-16 14:04:33 +0100

    auth redux store added

---
+++ client/src/api/auth.api.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const instance = axios.create({
+  baseURL: API_URL,
+});
+
+export interface IData {
+  email: string;
+  password: string;
+}
+
+export const userLogin = async (data: IData) => {
+  const res = (await instance.post('/login', data))?.data;
+  localStorage.setItem('currentUser', JSON.stringify(res.token));
+  localStorage.setItem('user', JSON.stringify(res.user));
+
+  return res;
+};
+
+export const userLogout = async () => {
+  localStorage.removeItem('currentUser');
+  localStorage.removeItem('user');
+  return null;
+};
